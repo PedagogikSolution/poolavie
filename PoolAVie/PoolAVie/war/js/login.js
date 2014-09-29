a58168cddafb95dbbc51b81c735a36f6eb933a9e
Francois Groulx <pedagogiksolution@gmail.com>
2014-09-29 18:26:14 -0400

    ajout de la fonction du bouton sign in

--- PoolAVie/PoolAVie/war/js/login.js
+++ PoolAVie/PoolAVie/war/js/login.js
@@ -7,3 +7,9 @@ function onLoginButtonClick() {
 function onLogoClick() {
 	location.reload();
 }
+
+function onSignInButtonClick() {
+	document.getElementById("pub_main_content_home").style.display="none";
+    document.getElementById("register_home").style.display="block";
+	
+}
