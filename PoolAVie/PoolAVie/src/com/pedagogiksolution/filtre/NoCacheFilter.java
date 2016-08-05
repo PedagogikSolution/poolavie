b9abe19a5f3f27433f327123f1397303fd9c6366
Francois Groulx <pedagogiksolution@gmail.com>
2016-08-05 13:09:05 -0400

    filtre nocache

---
+++ PoolAVie/PoolAVie/src/com/pedagogiksolution/filtre/NoCacheFilter.java
@@ -0,0 +1,45 @@
+package com.pedagogiksolution.filtre;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NoCacheFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+
+	HttpServletRequest request = (HttpServletRequest) req;
+	HttpServletResponse response = (HttpServletResponse) resp;
+
+	
+	   
+	   
+		response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+		response.setDateHeader("Expires", 0);
+		response.setHeader("Pragma", "No-cache");
+
+		chain.doFilter(request, response);
+	   
+	}
+
+
+    @Override
+    public void destroy() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
