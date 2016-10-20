41a7578a94ade7164e7dcb6096f58605dc53d085
Francois Groulx <pedagogiksolution@gmail.com>
2016-10-20 17:30:55 -0400

    Signature

--- PoolAVie/PoolAVie/src/com/pedagogiksolution/model/SignatureModel.java
+++ PoolAVie/PoolAVie/src/com/pedagogiksolution/model/SignatureModel.java
@@ -1,49 +1,263 @@
 package com.pedagogiksolution.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.pedagogiksolution.dao.PlayersDao;
+import com.pedagogiksolution.datastorebeans.Attaquant;
+import com.pedagogiksolution.datastorebeans.Defenseur;
+import com.pedagogiksolution.datastorebeans.Equipe;
+import com.pedagogiksolution.datastorebeans.Gardien;
+import com.pedagogiksolution.datastorebeans.Pool;
 import com.pedagogiksolution.datastorebeans.Utilisateur;
 
 public class SignatureModel {
+    List<Integer> players_id = new ArrayList<Integer>();
+    List<String> nom2 = new ArrayList<String>();
+    List<String> teamOfPlayer = new ArrayList<String>();
+    List<Integer> pj = new ArrayList<Integer>();
+    List<Integer> but_victoire = new ArrayList<Integer>();
+    List<Integer> aide_overtime = new ArrayList<Integer>();
+    List<Integer> blanchissage = new ArrayList<Integer>();
+    List<Integer> pts = new ArrayList<Integer>();
+    List<String> years_1 = new ArrayList<String>();
+    List<String> years_2 = new ArrayList<String>();
+    List<String> years_3 = new ArrayList<String>();
+    List<String> years_4 = new ArrayList<String>();
+    List<String> years_5 = new ArrayList<String>();
+    List<Integer> salaire_contrat = new ArrayList<Integer>();
     private PlayersDao playersDao;
-    
+
     public SignatureModel(PlayersDao playersDao) {
 	this.playersDao = playersDao;
     }
 
     public void putPlayersThatCanBeSignInBean(HttpServletRequest req) {
-	
+
 	Utilisateur mBeanUser = (Utilisateur) req.getSession().getAttribute("Utilisateur");
 	int teamId = mBeanUser.getTeamId();
 	int poolId = mBeanUser.getPoolId();
-	
-	
-	playersDao.getPlayersThatCanBeSign(teamId,poolId,req);
-	
+
+	playersDao.getPlayersThatCanBeSign(teamId, poolId, req);
+
     }
 
-    
+    @SuppressWarnings("unchecked")
     public void signatureAfterDraft(HttpServletRequest req) {
 	String nombreAnneeSignature = req.getParameter("nombreAnneeSignature");
 	int numberOfYear = Integer.parseInt(nombreAnneeSignature);
 	String draft_player_id = req.getParameter("draft_player_id");
-	String nom = req.getParameter("nom");
 	String position = req.getParameter("position");
-	String team = req.getParameter("team");
 	String salaire = req.getParameter("salaire");
 	Utilisateur mBeanUser = (Utilisateur) req.getSession().getAttribute("Utilisateur");
 	int teamId = mBeanUser.getTeamId();
 	int poolId = mBeanUser.getPoolId();
-	
+	MemcacheService memcache = MemcacheServiceFactory.getMemcacheService();
+	Key entityKey = null;
+	Key clefMemCache = null;
+	Attaquant mBeanAttaquant = null;
+	Defenseur mBeanDefenseur = null;
+	Gardien mBeanGardien = null;
+	Pool mBeanPool = (Pool) req.getSession().getAttribute("Pool");
+	String poolID = mBeanPool.getPoolID();
+	String datastoreID = poolID + "_" + teamId;
+	Entity entity = null, equipeEntity = null; 
+	Key equipeKey;
+	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
 	// A: on change la valeur des years_x dans la bdd players_x
-	playersDao.signPlayerAfterDraft(teamId,poolId,draft_player_id,salaire,numberOfYear);
-	
+	playersDao.signPlayerAfterDraft(teamId, poolId, draft_player_id, salaire, numberOfYear);
+
 	// B: on modifie les datatstore attaquant/def/goal/
-	
+
+	switch (position) {
+	case "attaquant":
+	    clefMemCache = KeyFactory.createKey("Attaquant", datastoreID);
+
+	    entityKey = KeyFactory.createKey("Attaquant", datastoreID);
+	    break;
+	case "defenseur":
+	    clefMemCache = KeyFactory.createKey("Defenseur", datastoreID);
+	    mBeanDefenseur = new Defenseur();
+	    entityKey = KeyFactory.createKey("Defenseur", datastoreID);
+	    break;
+	case "gardien":
+	    clefMemCache = KeyFactory.createKey("Gardien", datastoreID);
+	    mBeanGardien = new Gardien();
+	    entityKey = KeyFactory.createKey("Gardien", datastoreID);
+	    break;
+	}
+
+	try {
+
+	    entity = datastore.get(entityKey);
+
+	    players_id = (List<Integer>) entity.getProperty("players_id");
+
+	    int indexOfPlayersToSign = players_id.indexOf(draft_player_id);
+
+	    mapDatastoreInTempArray(entity, position);
+
+	    switch (numberOfYear) {
+	    case 1:
+		years_3.set(indexOfPlayersToSign, "X");
+		years_4.set(indexOfPlayersToSign, "X");
+		years_5.set(indexOfPlayersToSign, "X");
+
+		break;
+	    case 2:
+		years_2.set(indexOfPlayersToSign, salaire);
+		years_3.set(indexOfPlayersToSign, "X");
+		years_4.set(indexOfPlayersToSign, "X");
+		years_5.set(indexOfPlayersToSign, "X");
+		break;
+	    case 3:
+		years_2.set(indexOfPlayersToSign, salaire);
+		years_3.set(indexOfPlayersToSign, salaire);
+		years_4.set(indexOfPlayersToSign, "X");
+		years_5.set(indexOfPlayersToSign, "X");
+		break;
+	    case 4:
+		years_2.set(indexOfPlayersToSign, salaire);
+		years_3.set(indexOfPlayersToSign, salaire);
+		years_4.set(indexOfPlayersToSign, salaire);
+		years_5.set(indexOfPlayersToSign, "X");
+		break;
+
+	    case 5:
+		years_2.set(indexOfPlayersToSign, salaire);
+		years_3.set(indexOfPlayersToSign, salaire);
+		years_4.set(indexOfPlayersToSign, salaire);
+		years_5.set(indexOfPlayersToSign, salaire);
+		break;
+
+	    }
+	    datastore.put(entity);
+
+	    String jspName;
+	    switch (position) {
+	    case "attaquant":
+		jspName = "Attaquant" + teamId;
+		mBeanAttaquant = (Attaquant) req.getSession().getAttribute(jspName);
+		mBeanAttaquant.setAide_overtime(aide_overtime);
+		mBeanAttaquant.setBlanchissage(blanchissage);
+		mBeanAttaquant.setBut_victoire(but_victoire);
+		mBeanAttaquant.setNom(nom2);
+		mBeanAttaquant.setPj(pj);
+		mBeanAttaquant.setPts(pts);
+		mBeanAttaquant.setTeamOfPlayer(teamOfPlayer);
+		mBeanAttaquant.setYears_1(years_1);
+		mBeanAttaquant.setYears_2(years_2);
+		mBeanAttaquant.setYears_3(years_3);
+		mBeanAttaquant.setYears_4(years_4);
+		mBeanAttaquant.setYears_5(years_5);
+
+		memcache.put(clefMemCache, mBeanAttaquant);
+		break;
+	    case "defenseur":
+		jspName = "Defenseur" + teamId;
+		mBeanDefenseur = (Defenseur) req.getSession().getAttribute(jspName);
+		mBeanDefenseur.setAide_overtime(aide_overtime);
+		mBeanDefenseur.setBlanchissage(blanchissage);
+		mBeanDefenseur.setBut_victoire(but_victoire);
+		mBeanDefenseur.setNom(nom2);
+		mBeanDefenseur.setPj(pj);
+		mBeanDefenseur.setPts(pts);
+		mBeanDefenseur.setTeamOfPlayer(teamOfPlayer);
+		mBeanDefenseur.setYears_1(years_1);
+		mBeanDefenseur.setYears_2(years_2);
+		mBeanDefenseur.setYears_3(years_3);
+		mBeanDefenseur.setYears_4(years_4);
+		mBeanDefenseur.setYears_5(years_5);
+		memcache.put(clefMemCache, mBeanDefenseur);
+		break;
+	    case "gardien":
+		jspName = "Gardien" + teamId;
+		mBeanGardien = (Gardien) req.getSession().getAttribute(jspName);
+		mBeanGardien.setAide_overtime(aide_overtime);
+		mBeanGardien.setBlanchissage(blanchissage);
+		mBeanGardien.setBut_victoire(but_victoire);
+		mBeanGardien.setNom(nom2);
+		mBeanGardien.setPj(pj);
+		mBeanGardien.setPts(pts);
+		mBeanGardien.setTeamOfPlayer(teamOfPlayer);
+		mBeanGardien.setYears_1(years_1);
+		mBeanGardien.setYears_2(years_2);
+		mBeanGardien.setYears_3(years_3);
+		mBeanGardien.setYears_4(years_4);
+		mBeanGardien.setYears_5(years_5);
+		memcache.put(clefMemCache, mBeanGardien);
+		break;
+	    }
+
+	} catch (EntityNotFoundException e) {
+
+	}
+
 	// C: on modifie le datastore equipe
-	   	
+	
+	equipeKey = KeyFactory.createKey("Equipe", datastoreID);
+
+	    try {
+		equipeEntity = datastore.get(equipeKey);
+
+		
+
+		Long temp_nb_contrat = (Long) equipeEntity.getProperty("nb_contrat");
+		int nb_contrat =  temp_nb_contrat.intValue();
+		nb_contrat = nb_contrat + 1;
+		equipeEntity.setProperty("budget_restant", nb_contrat);
+		datastore.put(equipeEntity);
+
+		String jspName;
+		jspName = "Equipe" + teamId;
+		clefMemCache = KeyFactory.createKey("Equipe", datastoreID);
+		Equipe mBeanEquipe = new Equipe();
+		mBeanEquipe = (Equipe) req.getSession().getAttribute(jspName);
+
+		mBeanEquipe.setNb_contrat(nb_contrat);
+		memcache.put(clefMemCache, mBeanEquipe);
+
+	    } catch (EntityNotFoundException e) {
+
+	    }
+
     }
-    
+
+    @SuppressWarnings("unchecked")
+    private void mapDatastoreInTempArray(Entity entity, String position) {
+
+	List<Integer> testIfEmpty = (List<Integer>) entity.getProperty("players_id");
+
+	if (testIfEmpty != null) {
+
+	    players_id.addAll((List<Integer>) entity.getProperty("players_id"));
+	    nom2.addAll((List<String>) entity.getProperty("nom"));
+	    teamOfPlayer.addAll((List<String>) entity.getProperty("teamOfPlayer"));
+	    pj.addAll((List<Integer>) entity.getProperty("pj"));
+	    but_victoire.addAll((List<Integer>) entity.getProperty("but_victoire"));
+	    aide_overtime.addAll((List<Integer>) entity.getProperty("aide_overtime"));
+	    if (position.equalsIgnoreCase("Gardien")) {
+		blanchissage.addAll((List<Integer>) entity.getProperty("blanchissage"));
+	    }
+	    pts.addAll((List<Integer>) entity.getProperty("pts"));
+	    years_1.addAll((List<String>) entity.getProperty("years_1"));
+	    years_2.addAll((List<String>) entity.getProperty("years_2"));
+	    years_3.addAll((List<String>) entity.getProperty("years_3"));
+	    years_4.addAll((List<String>) entity.getProperty("years_4"));
+	    years_5.addAll((List<String>) entity.getProperty("years_5"));
+	    salaire_contrat.addAll((List<Integer>) entity.getProperty("salaire_contrat"));
+	}
+    }
 
 }
--- PoolAVie/PoolAVie/src/com/pedagogiksolution/servlet/SignatureServlet.java
+++ PoolAVie/PoolAVie/src/com/pedagogiksolution/servlet/SignatureServlet.java
@@ -21,45 +21,51 @@ public class SignatureServlet extends HttpServlet {
 
     public static final String CONF_DAO_FACTORY = "daofactory";
     private PlayersDao playersDao;
-    
+
     @Override
     public void init() throws ServletException {
 	/* Récupération d'une instance de notre nos DAO */
 	this.playersDao = ((DAOFactory) getServletContext().getAttribute(CONF_DAO_FACTORY)).getPlayersDao();
     }
-    
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	Pool mBeanPool = (Pool) req.getSession().getAttribute("Pool");
 	int cycleAnnuel = mBeanPool.getCycleAnnuel();
 	if (cycleAnnuel == 3) {
-	   	    
-	    DraftPlayersModel mModelDraft = new DraftPlayersModel();	    	   
-	    mModelDraft.putDatastoreIntoBean(mBeanPool,req);
+
+	    DraftPlayersModel mModelDraft = new DraftPlayersModel();
+	    mModelDraft.putDatastoreIntoBean(mBeanPool, req);
 	}
-	if (cycleAnnuel == 4){
-	    
+
+	String from = req.getParameter("from");
+	int fromId = Integer.parseInt(from);
+	if (cycleAnnuel == 4) {
+
 	    SignatureModel mModelSignature = new SignatureModel(playersDao);
 	    mModelSignature.putPlayersThatCanBeSignInBean(req);
 	    
+	    fromId=2;
+
 	}
-	
-	String from = req.getParameter("from");
-	int fromId = Integer.parseInt(from);
-	switch(fromId){
-	case 1: req.getRequestDispatcher("jsp/signature/managerCenter.jsp").forward(req, resp);
+	switch (fromId) {
+	case 1:
+	    req.getRequestDispatcher("jsp/signature/managerCenter.jsp").forward(req, resp);
 	    break;
-	case 2: req.getRequestDispatcher("jsp/signature/signatureAfterDraft.jsp").forward(req, resp);
+	case 2:
+	    req.getRequestDispatcher("jsp/signature/signatureAfterDraft.jsp").forward(req, resp);
 	    break;
-	case 3: req.getRequestDispatcher("jsp/signature/managerCenter.jsp").forward(req, resp);
+	case 3:
+	    req.getRequestDispatcher("jsp/signature/managerCenter.jsp").forward(req, resp);
 	    break;
-	case 4: req.getRequestDispatcher("jsp/signature/managerCenter.jsp").forward(req, resp);
+	case 4:
+	    req.getRequestDispatcher("jsp/signature/managerCenter.jsp").forward(req, resp);
 	    break;
-	case 5: req.getRequestDispatcher("jsp/signature/managerCenter.jsp").forward(req, resp);
+	case 5:
+	    req.getRequestDispatcher("jsp/signature/managerCenter.jsp").forward(req, resp);
 	    break;
-	    
+
 	}
-	
 
     }
 
@@ -68,9 +74,9 @@ public class SignatureServlet extends HttpServlet {
 	SignatureModel mModel = new SignatureModel(playersDao);
 	String signatureStep = req.getParameter("signatureStep");
 	int signatureStepId = Integer.parseInt(signatureStep);
-	
-	switch(signatureStepId){
-	case 1: 
+
+	switch (signatureStepId) {
+	case 1:
 	    // on persiste dans bdd et datastore et session les changements
 	    mModel.signatureAfterDraft(req);
 	    resp.sendRedirect("/Signature?from=2");
@@ -81,15 +87,9 @@ public class SignatureServlet extends HttpServlet {
 	    break;
 	case 4:
 	    break;
-		
+
 	}
-	
-	
-	
 
     }
-    
-    
-    
 
 }
