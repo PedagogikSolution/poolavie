d851bc876b4d966f2ea6bf8b80f49f25f1b8392c
Francois Groulx <pedagogiksolution@gmail.com>
2020-02-22 22:36:55 -0500

    bug agent recu

--- PoolAVie/PoolAVie/src/com/pedagogiksolution/dao/PlayersDaoImpl.java
+++ PoolAVie/PoolAVie/src/com/pedagogiksolution/dao/PlayersDaoImpl.java
@@ -1623,11 +1623,6 @@ public class PlayersDaoImpl implements PlayersDao {
 
 				mBeanEquipe.setTotal_salaire_now(mBeanEquipe.getTotal_salaire_now() + salaire);
 				mBeanEquipe.setBudget_restant(mBeanEquipe.getBudget_restant() - salaire);
-				if (mBeanEquipe.getBudget_restant() < 0) {
-					mBeanEquipe.setArgent_recu(mBeanEquipe.getArgent_recu() + mBeanEquipe.getBudget_restant());
-
-					mBeanEquipe.setBudget_restant(0);
-				}
 				mBeanEquipe.setNb_equipe(mBeanEquipe.getNb_equipe() + 1);
 				mBeanEquipe.setNb_defenseur(mBeanEquipe.getNb_defenseur() + 1);
 				mBeanEquipe.setManquant_def(mBeanEquipe.getManquant_def() - 1);
@@ -1726,11 +1721,6 @@ public class PlayersDaoImpl implements PlayersDao {
 
 				mBeanEquipe.setTotal_salaire_now(mBeanEquipe.getTotal_salaire_now() + salaire);
 				mBeanEquipe.setBudget_restant(mBeanEquipe.getBudget_restant() - salaire);
-				if (mBeanEquipe.getBudget_restant() < 0) {
-					mBeanEquipe.setArgent_recu(mBeanEquipe.getArgent_recu() + mBeanEquipe.getBudget_restant());
-
-					mBeanEquipe.setBudget_restant(0);
-				}
 				mBeanEquipe.setNb_equipe(mBeanEquipe.getNb_equipe() + 1);
 				mBeanEquipe.setNb_gardien(mBeanEquipe.getNb_gardien() + 1);
 				mBeanEquipe.setManquant_gardien(mBeanEquipe.getManquant_gardien() - 1);
